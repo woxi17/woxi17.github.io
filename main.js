1d52a1cd62832e8caa9b628d7bde86db213caaa5
RTavare <rohit.t797@yahoo.com>
2017-03-26 09:59:41 -0700

    main page

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $(".title-text").animate({"opacity" : '1.0'}, 1000);
+  $(".subtitle-text").animate({"opacity" : '1.0'}, 2000);
+  $("#dive").animate({"opacity" : '1.0'}, 3000);
+});
